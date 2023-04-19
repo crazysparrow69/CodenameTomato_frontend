ead23b2de1937959da28b92445fb112256f92147
den <denissytnikmacho@gmail.com>
2023-04-19 17:49:46 +0300

    added loading username from context

--- src/components/Main/Sidebar.js
+++ src/components/Main/Sidebar.js
@@ -1,12 +1,16 @@
 import styles from "./Sidebar.module.css";
+import AppContext from "../../context/app-context";
+import { useContext } from "react";
 
 const Sidebar = () => {
+  const ctx = useContext(AppContext);
+
   return (
     <div className={styles.sidebar}>
       <div className={styles["sidebar__avatar"]}>
         <img src="https://yummyanime.tv/bag.webp" alt="Error"></img>
       </div>
-      <div className={styles["sidebar__username"]}>Nagibator669</div>
+      <div className={styles["sidebar__username"]}>{ctx.userData.username}</div>
     </div>
   );
 };
