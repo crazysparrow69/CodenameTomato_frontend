11212df6741f06fb692f6cdc4a61dbf52115f25a
den <denissytnikmacho@gmail.com>
2023-04-20 18:35:12 +0300

    finalized modal component

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom";
 const Modal = (props) => {
   const modal = (
     <div className={styles["modal-wrapper"]}>
-      <div className={styles["modal-window"]}>{props.children}</div>
+      <div className={styles["modal-window"]}>
+        <div className={styles["modal-actions"]}>
+          <button onCLick={props.onClose}>✖</button>
+        </div>
+        {props.children}
+      </div>
       <div className={styles.overlay}></div>
     </div>
   );
