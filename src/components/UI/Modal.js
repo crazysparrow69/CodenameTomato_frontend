6f332377e9f79ba0127f715498997e3bea96dba5
den <denissytnikmacho@gmail.com>
2023-04-20 15:28:34 +0300

    created modal window component

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,21 @@
 import styles from "./Modal.module.css";
+import ReactDOM from "react-dom";
 
-const Modal = () => {
-  return 
+const Modal = (props) => {
+  const modal = (
+    <div className={styles["modal-wrapper"]}>
+      <div className={styles["modal-window"]}>{props.children}</div>
+      <div className={styles.overlay}></div>
+    </div>
+  );
+
+  const modalRoot = document.getElementById("modal-root");
+
+  return (
+    <>
+      {ReactDOM.createPortal(modal, modalRoot)}
+    </>
+  );
 };
 
 export default Modal;
