82e2c53c4ee495c4a3b072d4c8f713574e19963c
den <denissytnikmacho@gmail.com>
2023-04-24 20:05:17 +0300

    added rendering modal with task form

--- src/components/Main/Tasks.js
+++ src/components/Main/Tasks.js
@@ -2,9 +2,13 @@ import styles from "./Tasks.module.css";
 import Button from "../UI/Button";
 import TaskItem from "./TaskItem";
 import AppContext from "../../context/app-context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import Modal from "../UI/Modal";
+import TaskForm from "../Forms/TaskForm";
 
 const Tasks = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const ctx = useContext(AppContext);
 
   const taskItems = ctx.tasks.map((elem) => (
@@ -17,10 +21,21 @@ const Tasks = () => {
     />
   ));
 
+  const createTask = () => {
+    setShowModal(true);
+  };
+
+  const handleClosing = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className={styles.tasks}>
+      {showModal && <Modal onClose={handleClosing} >
+        <TaskForm />
+      </Modal>}
       <div className={styles["tasks__create"]}>
-        <Button text="Create new task" />
+        <Button text="Create new task" onClick={createTask} />
       </div>
       <div className={styles["tasks__list"]}>
         <ul>
